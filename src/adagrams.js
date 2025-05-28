757ec3b6f4f27c98e44fe9d7b299b265a26d4198
Ellie Alavi <elhamalavi.f@gmail.com>
2025-05-27 23:48:37 -0400

    Converted letterPool to a list and define a draw number out of its index

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,63 @@
+import { of } from "core-js/core/array";
+
+const HandSize = 10
+
+const letterPool = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+
+const SCORE_CHART = {
+  A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+  D: 2, G: 2,
+  B: 3, C: 3, M: 3, P: 3,
+  F: 4, H: 4, V: 4, W: 4, Y: 4,
+  K: 5,
+  J: 8, X: 8,
+  Q: 10, Z: 10
+};
+
+
+// export const makeDict = () => { };
+
+// Implement this method for wave 1
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  let letterPoolList =[]
+
+  for (let [letter, freq] of Object.entries(letterPool)) {
+    for (let i = 0; i < freq; i++) {
+      letterPoolList.push(letter)
+    }; 
+  };
+  
+  // const totalLetters = Object.values(letterPool).reduce((sum, count) => sum + count, 0);
+  const totalLetters = letterPoolList.length;
+  const draw = Math.floor(Math.random() * totalLetters);
+
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
